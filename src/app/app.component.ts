f3a060c3dd652b8af91d1ad4581ce63ed7c7a5a6
Mario Neubacher <neubacher.mario00@gmail.com>
2021-12-15 13:53:29 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  images = ['code.jpg','macbook.jpg'];
+  headlines = [
+    'Coding: the most important skill in the 21st century',
+    'Bringing engineering to the next level']
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length; //only as long as images length and begins again
+      this.showImage = false;
+
+      setTimeout(() => { //removes image and shows it again to prevent time loading differences in css and setIntervall()
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+}
