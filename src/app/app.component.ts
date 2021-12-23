b77a6b07b63f25aeaccc02f0a74dcc4bd1ab3952
Mario Neubacher <neubacher.mario00@gmail.com>
2021-12-23 15:35:36 +0100

    filterProjects function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
 import { ThisReceiver } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `<div #id-display>Some text</div>`
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  projects = [
+    {
+      img: '/assets/img/el-pollo-loco.png',
+      id: 'id-javascript'
+    },
+    {
+      img: '/assets/img/homepage.png',
+      id: 'id-angular'
+    }
+  ]
 
-  constructor(public router: Router) {} //"public" to access router from html 
+  filterProjects() {
+    
+    @ViewChild('id-display') myDiv: ElementRef;
+
+    ngAfterViewInit() {
+      console.log(this.myDiv.nativeElement.innerHTML);
+  }
+
+    for (let i = 0; i < projects.length; i++) {
+      const placeholder = projects[i];
+      if (placeholder['id'].includes('angular')) {
+        document.getElementById('id-display').innerHTML += `
+          <div id="display" class="div-portfolio">
+            <div style="background-image: url(${placeholder})"></div>
+            <div style="background-image: url(${placeholder})"></div>
+          </div>
+        `;
+      }if (placeholder['id'].includes('javascript')) {
+        document.getElementById('id-display').innerHTML += `
+          <div id="display" class="div-portfolio">
+            <div style="background-image: url(${placeholder})"></div>
+            <div style="background-image: url(${placeholder})"></div>
+          </div>
+        `;
+      } else {
+        document.getElementById('id-display').innerHTML += `
+            <div id="display" class="div-portfolio">
+              <div style="background-image: url(${placeholder})"></div>
+              <div style="background-image: url(${placeholder})"></div>
+            </div>
+          `;
+        }
+      }
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
     ImprintComponent,
     DataProtectionComponent
   ],
