1ae26901470f6e55f7c2283c1bc498e87b7514ad
Mario Neubacher <neubacher.mario00@gmail.com>
2022-03-24 17:14:49 +0100

    implement translateservice in technologies component

--- src/app/technologies/technologies.component.ts
+++ src/app/technologies/technologies.component.ts
@@ -1,75 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateCompiler } from '@ngx-translate/core';
 import * as Aos from 'aos';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-technologies',
   templateUrl: './technologies.component.html',
   styleUrls: ['./technologies.component.scss']
 })
+
 export class TechnologiesComponent implements OnInit {
 
-  technologies = [
-    {
-      img: 'typescript.png',
-      name: 'TypeScript',
-    },
-    {
-      img: 'material.png',
-      name: 'Material Design',
-    },
-    {
-      img: 'angular.png',
-      name: 'Angular',
-    },
-    {
-      img: 'bootstrap.png',
-      name: 'Bootstrap',
-    },
-    {
-      img: 'javascript.png',
-      name: 'JavaScript',
-    },
-    {
-      img: 'Group 205.png',
-      name: 'CSS',
-    },
-    {
-      img: 'html.png',
-      name: 'HTML',
-    },
-    {
-      img: 'scrum.png',
-      name: 'SCRUM',
-    },
-    {
-      img: 'Git-Icon-1788C 2.png',
-      name: 'Git',
-    },
-    {
-      img: 'Group 20.png',
-      name: translate.instant('design-thinking'), //{{ 'design-thinking' | translate }}
-    },
-    {
-      img: 'icons8-rest-api-80 2.png',
-      name: 'Rest API',
-    },
-    {
-      img: 'icons8-test-lab-96 2.png',
-      name: 'Test automation',
-    },
-    {
-      img: 'icons8-database-52 2.png',
-      name: 'Databases',
-    },
-    {
-      img: 'security.png',
-      name: 'IT-Security',
-    }
-  ]
+  technologies = [];
 
-  constructor() { }
-
+  constructor(private translateService: TranslateService) {
+    technologies = [{
+      {
+        img: 'typescript.png',
+        name: 'TypeScript',
+      },
+      {
+        img: 'material.png',
+        name: 'Material Design',
+      },
+      {
+        img: 'angular.png',
+        name: 'Angular',
+      },
+      {
+        img: 'bootstrap.png',
+        name: 'Bootstrap',
+      },
+      {
+        img: 'javascript.png',
+        name: 'JavaScript',
+      },
+      {
+        img: 'Group 205.png',
+        name: 'CSS',
+      },
+      {
+        img: 'html.png',
+        name: 'HTML',
+      },
+      {
+        img: 'scrum.png',
+        name: 'SCRUM',
+      },
+      {
+        img: 'Git-Icon-1788C 2.png',
+        name: 'Git',
+      },
+      {
+        img: 'Group 20.png',
+        name: translateService.instant('design-thinking'), //{{ 'design-thinking' | translate }}
+      },
+      {
+        img: 'icons8-rest-api-80 2.png',
+        name: 'Rest API',
+      },
+      {
+        img: 'icons8-test-lab-96 2.png',
+        name: 'Test automation',
+      },
+      {
+        img: 'icons8-database-52 2.png',
+        name: 'Databases',
+      },
+      {
+        img: 'security.png',
+        name: 'IT-Security',
+      },
+    }]
+  }
+  
   ngOnInit(): void {
     Aos.init();
   }
