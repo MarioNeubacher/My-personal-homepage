518880880550cc540aa71d4f564d53ff830a3474
Mario Neubacher <neubacher.mario00@gmail.com>
2022-04-05 16:22:41 +0200

    get html location from other component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,8 +3,9 @@ import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { DarkmodeService } from '../darkmode.service';
 import { ScrollingService } from '../scrolling.service';
-import {MatDialog } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { PopUpComponent } from '../pop-up/pop-up.component';
+import { InViewPortService } from '../in-view-port.service';
 
 @Component({
   selector: 'app-menu',
@@ -19,7 +20,14 @@ export class MenuComponent implements OnInit {
   @Input() selectedSection = true;
   darkmodeToggle;
 
-  constructor(public router: Router, public scrolling: ScrollingService, private translateService: TranslateService, public darkmodeService: DarkmodeService, public dialog: MatDialog) { }
+  constructor(
+    public router: Router,
+    public scrolling: ScrollingService,
+    private translateService: TranslateService,
+    public darkmodeService: DarkmodeService,
+    public dialog: MatDialog,
+    public inViewPortService: InViewPortService
+  ) { }
 
   ngOnInit(): void {
     /* this.darkMode = this.darkModeService.toggleDarkMode(); */
@@ -45,7 +53,7 @@ export class MenuComponent implements OnInit {
   openDialog(): void {
     const dialogRef = this.dialog.open(PopUpComponent);
 
-   
+
   }
 
 }
