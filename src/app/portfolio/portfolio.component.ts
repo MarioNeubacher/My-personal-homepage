111c579a180be30d44d345e3b649baea1701308c
Mario Neubacher <neubacher.mario00@gmail.com>
2022-08-29 17:44:47 +0200

    cant open elpolloloco

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -38,7 +38,7 @@ export class PortfolioComponent implements OnInit, AfterViewInit {
       category: 'javascript',
       title: 'el-pollo-loco',
       JsDoc: '',
-      app_link: 'https://mario-neubacher.developerakademie.com/2.%20Sakura%20Ramen/index.html',
+      app_link: 'https://mario-neubacher.developerakademie.com/elpolloloco/index.html',
       github_link: 'https://github.com/MarioNeubacher/El-Pollo-Loco',
     },
     {
